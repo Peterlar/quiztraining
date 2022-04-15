9062bb61f934780e17772f4a8a2b883fe2136395
Peterlar <Peter_kjaer@hotmail.com>
2022-04-15 21:54:11 +0000

    pushing code

--- script.js
+++ script.js
@@ -1,56 +1,129 @@
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
 
-let currentQuestion = {};
-let acceptingAnswers = false;
-let score = 0;
-let questionCounter = 0;
-let availableQuesions = [];
-  
-let Questions = [
-    {
-      question: "Which year was the first nba game played?",
-      answers: {
-        optn1: "1944",
-        optn2: "1945",
-        optn3: "1946",
-        optn4: "1947",
-      },
-      correctAns: 3,
-    },
+const questionContinerElement = document.getElementById('question-container')
+const questionElement = document.getElementById(question);
+const answerButtonsElement = document.getElementById('answer-buttons')
 
+let shuffledQuestions,currectQuestionIndex;
+let quizScore =0;
+
+
+startButton.addEventListener('click, startGame')
+
+nextButton.addEventListener('click') ,() =>{
+    currectQuestionIndex++
+    setnextQuestion()
+}
+       
+function startGame(){
+    startButton.classList.add('hide')
+    shuffledQuestions=questions.sort(() =>Math.random() -0.5)
+    currectQuestionIndex=0;
+    questionContinerElement.classList.remove('hide')
+    setnextQuestion()
+    quizScore=0
+
+}
+
+function setnextQuestion() {
+    resetState();
+    showQuestion(shuffledQuestions[currectQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText= question.question;
+    question.answers.forEach((answer) =>{
+        const button =document.createElement('button')
+        button.innerText=answer.text;
+        button.classList.add('btn')
+        if(answer.correct) {
+            button.dataset.correct =answer.correct
+        }
+        button.addEventListener('click' ,selectAnswer)
+        answerButtonsElement.appendChild(button)
+
+    })
+
+}
+
+function resetState(){
+    clearStatusClass(document.body)
+    nextButton.classList.add ('hide')
+    while(answerButtonsElement.firstchild) {
+
+    }
+}
+
+function selectAnswer (e) {
+    const selectedButton=e.target
+    const correct =selectedButton.dataset.correct
+
+    setStatusClass(document.body,correct),
+    Array.from(answerButtonsElement.children).forEach((button)=>{
+        setStatusclass(button,button.dataset.correct)
+    })
+    if(shuffledQuestions.lengt > currectQuestionIndex +1){
+        nextButton.classList.remove("hide")
+    }else {
+        startButton.innerText ="restart"
+        startButton.classList.remove("hide")
+    }if(selectedButton.dataset = correct) {
+        quizScore++
+    }
+    document.getElementById('right-answers').innerText=quizScore
+}
+
+function setStatusclass(element,correct){
+    clearStatusClass(element)
+    if(correct) {
+        element.classList("correct")
+    }else {
+        element.classList.add("wrong")
+    }
+}
+
+
+
+function clearStatusClass(element){
+    element.classlist.remove('correct')
+    element.classlist.remove('wrong')
+}
+const questions =[
     {
-      question:
-          "Who have scored most points in nba history?",
-        answers: {
-          optn1: "Michael Jordan",
-          optn2: "Lebron james",
-          optn3: "Karem Abdul-Jabbar",
-          optn4: "Kobe Bryant",
-        },
-        correctAns: 3,
+        question: 'vad heter jag',
+        answers : [
+            { text: 'Peter', correct: true},
+            { text: 'Peter', correct: false},
+            { text: 'Peter', correct: false},
+            { text: 'Peter', correct: false},
+        ],
     },
     {
-        question: "Which year was the first nba game played?",
-        answers: {
-          optn1: "1944",
-          optn2: "1945",
-          optn3: "1946",
-          optn4: "1947",
-        },
-        correctAns: 3,
-      },
-  
-      {
-        question:
-            "Who have scored most points in nba history?",
-          answers: {
-            optn1: "Michael Jordan",
-            optn2: "Lebron james",
-            optn3: "Karem Abdul-Jabbar",
-            optn4: "Kobe Bryant",
-          },
-          correctAns: 3,
-        },
-    ];
+        question: 'vad heter jag i efternamn?',
+        answers : [
+            { text: 'Bengt', correct: false},
+            { text: 'Anders', correct: false},
+            { text: 'Larsson', correct: true},
+            { text: 'Erik', correct: false},
+        ],
+    },
+    {
+        question: 'vad heter jag i efternamn?',
+        answers : [
+            { text: 'Bengt', correct: false},
+            { text: 'Anders', correct: false},
+            { text: 'Larsson', correct: true},
+            { text: 'Erik', correct: false},
+        ],
+    },
+    {
+        question: 'vad heter jag i efternamn?',
+        answers : [
+            { text: 'Svensson', correct: false},
+            { text: 'Sune', correct: false},
+            { text: 'Keff', correct: false},
+            { text: 'Kjaer', correct: true},
+        ],
+    },
+]
